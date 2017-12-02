f77d1a754dd7cf2905a6896b67e2eccbb2d9e018
Ronald Leppink <ronaldleppink@gmail.com>
2017-12-02 12:29:28 +0100

    Add day 2-2

--- day02/src/Main.hs
+++ day02/src/Main.hs
@@ -4,15 +4,15 @@ module Main where
 main :: IO ()
 main = do
   rawInput <- readFile "input"
-  let input = map words (lines rawInput)
 
-  print (checksum input)
+  let input = map (map read . words) (lines rawInput)
+
+  print (sum $ map maxDiff   input)  -- Day 2.1
+  print (sum $ map evenlyDiv input)  -- Day 2.2
 
 
-checksum :: [[String]] -> Int
-checksum xs = sum $ map maxDiff xs
+maxDiff :: [Int] -> Int
+maxDiff xs = maximum xs - minimum xs
 
-maxDiff :: [String] -> Int
-maxDiff xs = maximum ixs - minimum ixs
-  where ixs = map read xs
-
+evenlyDiv :: [Int] -> Int
+evenlyDiv xs = head [ div x y | x <- xs, y <- xs, mod x y == 0, x /= y ]
