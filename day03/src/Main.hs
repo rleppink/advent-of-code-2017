8f4230643348fb816320ff7eae8d57e0d308dfe3
Ronald Leppink <ronaldleppink@gmail.com>
2017-12-03 13:58:53 +0100

    Add day 3-2

--- day03/src/Main.hs
+++ day03/src/Main.hs
@@ -1,11 +1,17 @@
 module Main where
 
+import Data.Matrix
+
+
 main :: IO ()
 main = do
 
   -- Day 3.1
   -- This should be split into functions
   print (minimumSteps input + (input - rowMid [gridSize input - maximumSteps input..gridSize input]))
+
+  -- Day 3.2
+  print (foldr (\ a b -> extendFill b) startingMatrix [0..3])
 
 
 input :: Int
@@ -29,3 +35,60 @@ minimumSteps x = div (maximumSteps x) 2
 
 rowMid :: [Int] -> Int
 rowMid xs = xs !! div (length xs) 2
+
+
+-- Day 3.2
+
+startingMatrix :: Matrix Int
+startingMatrix = matrix 1 1 ( \ (_, _) -> 1 )
+
+zeroExtend :: Matrix Int -> Matrix Int
+zeroExtend x =
+  matrix
+    newSize
+    newSize
+    ( \ (row, col) ->
+        if row == 1 || col == 1 || row == newSize || col == newSize
+        then 0
+        else getElem (row-1) (col-1) x)
+  where newSize = nrows x + 2
+
+
+extendFill :: Matrix Int -> Matrix Int
+extendFill x =
+  foldl
+    (flip fillStep)
+    extended
+    (matrixFillSteps (nrows extended))
+  where extended = zeroExtend x
+
+-- | Where x = maximum size of matrix
+matrixFillSteps :: Int -> [(Int, Int)]
+matrixFillSteps x =
+     zip [x-1, x-2..1] (repeat x)
+  ++ zip (repeat 1)    [x-1, x-2..1]
+  ++ zip [2..x]        (repeat 1)
+  ++ zip (repeat x)    [2..x]
+
+fillStep :: (Int, Int) -> Matrix Int -> Matrix Int
+fillStep (x, y) z =
+  unsafeSet (sumAdjacent (x, y) z) (x, y) z
+
+sumAdjacent :: (Int, Int) -> Matrix Int -> Int
+sumAdjacent (x, y) z =
+    zeroGetElem (x - 1, y)     z
+  + zeroGetElem (x - 1, y - 1) z
+  + zeroGetElem (x    , y - 1) z
+  + zeroGetElem (x + 1, y - 1) z
+  + zeroGetElem (x + 1, y    ) z
+  + zeroGetElem (x + 1, y + 1) z
+  + zeroGetElem (x    , y + 1) z
+  + zeroGetElem (x - 1, y + 1) z
+
+zeroGetElem :: (Int, Int) -> Matrix Int -> Int
+zeroGetElem (x, y) z
+  | x < 1       = 0
+  | y < 1       = 0
+  | x > ncols z = 0
+  | y > nrows z = 0
+  | otherwise   = getElem x y z
