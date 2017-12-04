6ccea5bec318367bd3cac37454685fcb12ec3e45
Ronald Leppink <ronaldleppink@gmail.com>
2017-12-04 08:53:34 +0100

    Add day 4-2

--- day04/src/Main.hs
+++ day04/src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.List
 import Data.List.Unique
 
 
@@ -8,5 +9,14 @@ main = do
   rawInput <- readFile "input"
   let input = map (words) (lines rawInput)
 
+  -- Day 4.1
   print (foldr (\ a b -> if allUnique a then b + 1 else b) 0 input)
+
+  -- Day 4.2
+  print
+    (foldr
+      (\ a b -> if allUnique (map (sort) a) then b + 1 else b)
+      0
+      input)
+         
 
