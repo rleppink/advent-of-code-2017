4498f253a5ff28632c3c6b6cb32e90fdb12b0c8f
Ronald Leppink <ronaldleppink@gmail.com>
2017-12-05 13:35:29 +0100

    Add day 5

---
+++ day05/src/Main.hs
@@ -0,0 +1,39 @@
+module Main where
+
+
+import qualified Data.Sequence as S
+
+
+main :: IO ()
+main = do
+  rawInput <- readFile "input"
+  let input = map (\ a -> read a :: Int) (lines rawInput)
+
+  -- Day 5.1
+  -- print (stepList 0 0 input)
+
+  -- Day 5.2
+  print (stepList' 0 0 (S.fromList input))
+
+
+stepList :: Int -> Int -> [Int] -> Int
+stepList _ _ [] = 0
+stepList acc pos xs =
+  if nextPos > length xs - 1 || nextPos < 0
+  then acc + 1
+  else stepList (acc + 1) nextPos nextXs
+  where
+    nextPos = pos + (xs !! pos)
+    nextXs  = take pos xs ++ [(xs !! pos) + 1] ++ drop (pos + 1) xs
+
+
+stepList' :: Int -> Int -> S.Seq Int -> Int
+stepList' acc pos xs =
+  if nextPos > S.length xs - 1 || nextPos < 0
+  then acc + 1
+  else stepList' (acc + 1) nextPos nextXs
+  where
+    currentVal = S.index xs pos
+    nextPos    = pos + currentVal
+    nextXs     = S.update pos (currentVal + increment) xs
+    increment  = if currentVal >= 3 then -1 else 1
