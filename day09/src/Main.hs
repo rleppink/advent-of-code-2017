f905bf396bf63801b2b9d2ae802f37e73dc9cb39
Ronald Leppink <ronaldleppink@gmail.com>
2017-12-10 14:40:56 +0100

    Add day 9-2

--- day09/src/Main.hs
+++ day09/src/Main.hs
@@ -2,16 +2,19 @@ module Main where
 
 
 main :: IO ()
-main = fmap solve (readFile "src/input") >>= print
+main = fmap solve' (readFile "src/input") >>= print
 
 
 solve :: String -> Int
 solve x = score 0 0 (readGroups x "")
 
+solve' :: String -> Int
+solve' x = readGroups' x 0
+
 dropGarbage :: String -> String -> String
 dropGarbage x y =
   case safeHead x of
-    Nothing -> y
+    Nothing  -> y
     Just val ->
       case val of
         '!' -> dropGarbage (drop 2 x) y
@@ -46,3 +49,26 @@ score x y z =
         '{' -> score (x + 1) y       (tail z)
         '}' -> score (x - 1) (y + x) (tail z)
         une -> error ("Unexpected character while counting: " ++ [une])
+
+readGroups' :: String -> Int -> Int
+readGroups' x y =
+  case safeHead x of
+    Nothing  -> y
+    Just val ->
+      case val of
+        '!' -> readGroups'  (drop 2 x) y
+        '<' -> countGarbage (tail   x) y
+        ',' -> readGroups'  (tail   x) y
+        '{' -> readGroups'  (tail   x) y
+        '}' -> readGroups'  (tail   x) y
+        une -> error ("Unexpected character: " ++ [une])
+
+countGarbage :: String -> Int -> Int
+countGarbage x y =
+  case safeHead x of
+    Nothing  -> y
+    Just val ->
+      case val of
+        '!' -> countGarbage (drop 2 x) y
+        '>' -> readGroups'  (tail   x) y
+        _   -> countGarbage (tail   x) (y + 1)
